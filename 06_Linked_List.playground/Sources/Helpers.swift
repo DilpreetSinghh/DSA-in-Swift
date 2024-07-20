29e0a621c0054f743660431fcc827ecdc89dbc8d
Dilpreet Singh <dilpreetsinghhere@gmail.com>
2024-07-20 10:54:17 +0530

    Linked List: Just Code Added Code from Raywenderlich github's repo Adding Documentation later while reading Book

---
+++ 06_Linked_List.playground/Sources/Helpers.swift
@@ -0,0 +1,12 @@
+//
+//  Helpers.swift
+//  
+//
+//  Created by Dilpreet Singh on 20/07/24.
+//
+
+public func example(of description: String, action: () -> Void) {
+  print("---Example of \(description)---")
+  action()
+  print()
+}
